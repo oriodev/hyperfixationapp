8a918b39d3e89c29e1296ff32e2bfd679bac2c34
oriodev <oriodev@duck.com>
2025-08-19 12:53:48 +0100

    [003-port-auth-utilities] refactored auth apis
    - refactored auth apis such that they can be called as an api via HTTP requests

--- types.ts
+++ types.ts
@@ -1,4 +1,21 @@
 // TYPES
+export type User = {
+  id: string;
+  username: string;
+  email: string;
+  hashedPassword: string;
+}
+
+export type LoginData = {
+  email: string;
+  password: string;
+}
+
+export type SignupData = {
+  username: string;
+  email: string;
+  password: string;
+}
 
 // ENUMS
 export enum InputType {
