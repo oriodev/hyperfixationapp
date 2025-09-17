68b5e4a9b79395df78ac3b93b1aea08b029cd9f7
Maisalae <62596769+ybettles@users.noreply.github.com>
2025-09-17 23:08:35 +0100

    [005-connect-database] Update tests.

--- __tests__/db.tests.ts
+++ __tests__/db.tests.ts
@@ -1,33 +1,42 @@
-import { emailExists, usernameExists } from "../utils/db.utils";
+import { emailExists, getUserId, usernameExists } from "../utils/db.utils";
 
-describe('emailExists', () => {
+describe("emailExists", () => {
   it("should return the correct result for email which doesn't exist", async () => {
-    const userExists = await emailExists('notarealemail@email.com');
+    const userExists = await emailExists("notarealemail@email.com");
     expect(userExists).toBeFalsy();
   });
 
   it("should return the correct result for email which does exist", async () => {
-    const userExists = await emailExists('ypbettles@gmail.com');
+    const userExists = await emailExists("ypbettles@gmail.com");
     expect(userExists).toBeTruthy();
   });
 
-  // it("should throw an error if no email is provided", async () => {
-  //   expect(emailExists(undefined)).toThrow();
-  // });
+  it("should throw an error if no email is provided", async () => {
+    await expect(emailExists(undefined)).rejects.toThrow("No email provided.");
+  });
 });
 
-describe('usernameExists', () => {
+describe("usernameExists", () => {
   it("should return the correct result for username which doesn't exist", async () => {
-    const userExists = await usernameExists('notarealusername');
+    const userExists = await usernameExists("notarealusername");
     expect(userExists).toBeFalsy();
   });
 
   it("should return the correct result for username which does exist", async () => {
-    const userExists = await usernameExists('yasbettles');
+    const userExists = await usernameExists("yasbettles");
     expect(userExists).toBeTruthy();
   });
 
-  // it("should throw an error if no email is provided", async () => {
-  //   expect(usernameExists(undefined)).toThrow();
-  // });
+  it("should throw an error if no email is provided", async () => {
+    await expect(usernameExists(undefined)).rejects.toThrow(
+      "No username provided."
+    );
+  });
+});
+
+describe("getUserId", () => {
+  it("should return the correct user id for an email we know to exist", async () => {
+    const userId = await getUserId("ypbettles@gmail.com");
+    expect(userId).toBe("1");
+  });
 });
