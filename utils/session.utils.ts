6433d4489692bea5e60e3eae490ceb5234d7fa12
Maisalae <62596769+ybettles@users.noreply.github.com>
2025-09-17 18:57:05 +0100

    [005-connect-database] Add TODO to session.utils.ts
    So that we don't forget to do the thing.

--- utils/session.utils.ts
+++ utils/session.utils.ts
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 export const generateToken = async (userId: string) => {
-  const SECRET_KEY = 'CHANGETHISLATERANDMOVETOENV'; // MOVE TO ENV
+  const SECRET_KEY = 'CHANGETHISLATERANDMOVETOENV'; // TODO: MOVE TO ENV
   const token = jwt.sign({ id: userId }, SECRET_KEY, { expiresIn: '1h' });
 
   return token;
