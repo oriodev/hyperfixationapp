b3fe9b5313233765e5390d0ae6b1585109125f9a
Maisalae <62596769+ybettles@users.noreply.github.com>
2025-09-18 13:50:36 +0100

    [006-supabase-integration] Add supabase connection option to db.ts
    Updated example.env to contain the env vars and explain their usage.

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,29 @@
-import { Pool } from "pg";
+import { Pool, PoolConfig } from "pg";
 import 'dotenv/config';
 
-const dbConfig = {
-  user: process.env.POSTGRES_USER ?? "admin",
-  host: process.env.DB_HOST ?? "localhost",
-  database: process.env.POSTGRES_DB ?? "hyperfixation",
-  password: process.env.POSTGRES_PASSWORD,
-  port: Number(process.env.POSTGRES_PORT) ?? 5432,
-};
+let dbConfig: PoolConfig;
+// Should default to using local DB if env var is undefined.
+const useLocalDb = process.env.USE_LOCAL_DB ?? true;
+if (useLocalDb) {
+  dbConfig = {
+    user: process.env.POSTGRES_USER ?? "admin",
+    host: process.env.POSTGRES_HOST ?? "localhost",
+    database: process.env.POSTGRES_DB ?? "hyperfixation",
+    password: process.env.POSTGRES_PASSWORD,
+    port: Number(process.env.POSTGRES_PORT) ?? 5432,
+  };
+} else {
+  // If USE_LOCAL_DB is false, we require SUPABASE_URL to be defined, so that 
+  // we may connect to the cloud-hosted database. If none is given,
+  // we cannot provide a sensible default for this so we throw an error.
+  const connectionString = process.env.SUPABASE_URL;
+  if (!connectionString) {
+    throw new Error('No supabase connection URL found.');
+  }
+  dbConfig = {
+    connectionString,
+  }
+}
 
 export const pool = new Pool(dbConfig);
 
