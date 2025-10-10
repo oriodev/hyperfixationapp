59cbe77a71a71e28c2d47738258cf94a035f41f7
oriodev <oriodev@duck.com>
2025-10-10 14:47:07 +0100

    [004-profile-page-design]: Removes a line

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import { generateToken } from "@/utils/session.utils";
+import { NextResponse } from "next/server";
+import { comparePasswords } from "@/utils/auth.utils";
+import { User, LoginData } from "@/types";
+import { getUser } from "@/utils/db.utils";
+
+export const POST = async (request: Request) => {
+  const user: LoginData = await request.json();
+  const { email, password } = user;
+
+  try {
+    const dbResponse = await getUser(email);
+    const row = dbResponse.rows[0];
+
+    const currentUser: User = {
+      id: row.id,
+      username: row.username,
+      email: row.email,
+      hashedPassword: row.hashed_password,
+    };
+    const match = await comparePasswords(password, currentUser.hashedPassword);
+
+    if (!currentUser.id) {
+      return NextResponse.json(
+        { error: "Invalid credentials" },
+        { status: 401 }
+      );
+    }
+
+    if (!match) {
+      return NextResponse.json(
+        { error: "Invalid credentials" },
+        { status: 401 }
+      );
+    }
+
+    const token = await generateToken(currentUser.id);
+    return NextResponse.json({ token }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "INTERNAL SERVER ERROR" },
+      { status: 500 }
+    );
+  }
+};
